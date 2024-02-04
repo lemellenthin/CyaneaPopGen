664bd96da28cd63ac2b8640d44c16beb583bd20b
lemellenthin <lemellenthin@gmail.com>
2024-02-03 19:54:39 -0500

    novoplasty

--- CyaneaPopGen/Mitochondria/novoplasty/batch_novoplasty.sh
+++ CyaneaPopGen/Mitochondria/novoplasty/batch_novoplasty.sh
@@ -2,11 +2,12 @@
 
 #SBATCH --job-name=novoplasty
 #SBATCH --output=novoplasty.txt
-#SBATCH --time=1-00:00:00
-#SBATCH --partition=day
-#SBATCH --nodes=1                    # number of cores and nodes
-#SBATCH --cpus-per-task=18           # number of cores
-#SBATCH --mem-per-cpu=5G             # shared memory, scaling with CPU request
+#SBATCH --requeue
+#SBATCH --time=2-00:00:00
+#SBATCH --partition=ycga_bigmem
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem-per-cpu=10G
 
 
 module load miniconda
@@ -14,3 +15,5 @@ module load miniconda
 conda activate novoplasty
 
 NOVOPlasty4.3.1.pl -c config.txt
+
+conda deactivate
